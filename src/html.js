43a946b4cd3a11c6d2415f7ebe5c34a24a9cb59d
Michael Bykov <m.bykov@gmail.com>
2020-08-13 17:16:18 +0300

    i

---
+++ src/html.js
@@ -0,0 +1,16 @@
+'use strict'
+
+export const htmlChunk = `
+
+<p>some text</p>
+<p>some text </p>
+<p>with alink <a href="xxx#yyy">zzz</a><</p>
+<p class="ptext">p-text-par</p>
+<ul class="list">
+  <li>line string 1</li>
+  <li>line string 2</li>
+</ul>
+
+`
+
+// practical value.<a id="FNanchor_76_76"/><a href="chapters/item19/OEBPS/@public@vhost@g@gutenberg@html@files@39508@39508-h@39508-h-14.htm.html#Footnote_76_76" class="fnanchor pginternal">[76]</a></p>
--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@
 import _ from 'lodash'
 import EPub from 'epub'
 const cheerio = require('cheerio')
+// const htmlparser2 = require('htmlparser2')
+
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
 
 const iso6393 = require('iso-639-3')
 const log = console.log
+import { htmlChunk} from './html'
 
 export async function epub2md(bpath) {
   let epub = await getEpub(bpath)
@@ -15,7 +20,21 @@ export async function epub2md(bpath) {
   if (iso) lang = iso.iso6393
   let descr = {author: meta.creator, title: meta.title, lang: lang} // , description: meta
   let mds = await getMDs(epub)
+  // let mds = []
 
+  // let html = '<html><p>some text </p><p><a href="xxx#yyy" class="www">zzz</a></p><p class="ptext">p-text-par</p></html>'
+  // // let ahtml = dom.window.document.querySelector("a").outerHTML
+  // let el = q(html, 'a')
+  // log('_A-html', el.outerHTML)
+  // log('_A-href', el.getAttribute('href'))
+
+  // let $ = cheerio.load(html)
+  // log('_HTML', $.html())
+  // let ahref = cheerio.html($('a'))
+  // // let alink = $('a').attr('href')
+  // log('_A_HREF', ahref)
+
+  // log('_APPLE', $('.www').attr('id', 'favorite').html())
   return {descr: descr, mds: mds, imgs: []}
 }
 
@@ -30,27 +49,46 @@ function getEpub(bpath) {
   })
 }
 
+// пока что только tags: h, p, ul, img ???, todo: table
 async function getMDs(epub) {
+  let docs = []
   let ids = []
   let ftns = []
   for await (let chapter of epub.flow) {
-    // if (chapter.id != 'item11') continue
+    if (chapter.id != 'item11') continue
     let html  = await getChapter(epub, chapter.id)
+    html = htmlChunk.trim()
     // log('_HTML', chapter.id, '====================================\n', chapter.id, html)
-    const $ = cheerio.load(html)
 
-    $('p').each(function(i, elem) {
-      let text = $(this).text()
-      if (/76/.test(text)) {
-        log('_P', text)
+    const frag = JSDOM.fragment(html)
+    let children = frag.children
+    _.each(children, el=> {
+      log('_CH', el.nodeName)
+      if (el.nodeName == 'P') {
+
+      } else if (el.nodeName == 'UL') {
+        let olines = el.children
+        _.each(olines, el=> {
+
+        })
+      }
+      let footnote = el.querySelector('a')
+      if (footnote) {
+        log('_A-FN', footnote.outerHTML)
       }
     })
+    log('_CHs', children.length)
 
     ids.push(chapter.id)
   }
   log('_IDS', ids)
 
   return ['kuku']
+}
+
+function q(html, selector) {
+  const frag = JSDOM.fragment(html)
+  return frag.querySelector(selector)
 }
 
 function getChapter(epub, id) {
