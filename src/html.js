765bc8995f78b94c0a8015e740f8c3804853ab83
Michael Bykov <m.bykov@gmail.com>
2020-08-15 02:02:20 +0300

    i

--- src/html.js
+++ src/html.js
@@ -2,10 +2,13 @@
 
 export const htmlChunk = `
 
+<H1 id="id_0">_HEADER _ some header</p>
 <p id="id_1">par some text</p>
 <p id="id_2">par some other text </p>
 <p id="id_3">par with alink <a id="aid_1" href="xxx#fn_1">[[z1]]</a></p>
 <p id="id_4" class="ptext">par p-text-par <a id="aid_2" href="xxx#fn_2">z2</a></p>
+<p id="id_4-1" class="ptext">par p-text-par <a id="aid_2" href="xxx#linknote-11">z2</a></p>
+<p id="id_4-2" class="ptext">par p-text-par <a id="aid_2" href="xxx#linknote-12">z2</a></p>
 <ul id="ulid_1" class="list">
   <li id="liid_1">line string 1</li>
   <li id="liid_2">line string 2</li>
@@ -13,6 +16,16 @@ export const htmlChunk = `
 <p id="fn_1"> footnote fn_1 text </p>
 <p id="fn_2"> footnote fn_2 text </p>
 <p id="id_2">par some other text </p>
+
+<p>in astronomy and physics generally.</p>
+<div class="fig c4"><a id="illus03"/> “QUADRANS MURALIS SIVE TICHONICUS.”
+<p class="caption">“Q<small>UADRANS</small> M<small>URALIS SIVE</small> T<small>ICHONICUS</small>.”<br/>
+With portrait of Tycho Brahe, instruments, etc., painted on the wall; showing assistants using the sight, watching the clock, and recording. (From the author’s copy of the <i>Astronomiæ Instauratæ Mechanica</i>.)</p>
+</div>
+<hr/>
+<p><b>FOOTNOTES:</b></p>
+<p class="footnote"><a id="linknote-11"/> <a href="chapters/item11/OEBPS/@public@vhost@g@gutenberg@html@files@8172@8172-h@8172-h-6.htm.html#linknoteref-11" class="pginternal">[1]</a> For definition see p. 22.</p>
+<p class="footnote"><a id="linknote-12"/> <a href="chapters/item11/OEBPS/@public@vhost@g@gutenberg@html@files@8172@8172-h@8172-h-6.htm.html#linknoteref-12" class="pginternal">[2]</a> <i>Ibid</i>.</p>
 
 `
 
--- src/index.js
+++ src/index.js
@@ -20,7 +20,12 @@ export async function epub2md(bpath) {
   let iso = _.find(iso6393, iso=> iso.iso6391 == lang)
   if (iso) lang = iso.iso6393
   let descr = {author: meta.creator, title: meta.title, lang: lang} // , description: meta
-  let docs = await getMDs(epub)
+  let docs
+  try {
+    docs = await getMDs(epub)
+  } catch(err) {
+    docs = []
+  }
   // log('_META', epub.toc)
   return {descr, docs, imgs: []}
 }
@@ -59,22 +64,24 @@ async function getMDs(epub) {
   for await (let flowchapter of epub.flow) {
     // log('_CH ID:', flowchapter.id)
     // continue
-    if (flowchapter.id != 'item5') continue // astronomy
+    if (flowchapter.id != 'item11') continue // astronomy
     // if (flowchapter.id != 'c06') continue // hindus
 
     let html  = await getChapter(epub, flowchapter.id)
-    // html = htmlChunk.trim()
+    html = htmlChunk.trim()
     // log('_HTML', flowchapter.id, '====================================\n', flowchapter.id, html)
+    html = html.replace(/\/>/g, '/></a>')
 
     let docid = 0
     let chapter = []
     const dom = new JSDOM(html)
+    // dom = new JSDOM(html, {contentType: "application/xhtml+xml"})
+    // dom = new JSDOM(html, {contentType: "text/html"})
     walk(dom.window.document.body.childNodes, function (node) {
-      // log('_NODE:', node.nodeName, node.id)
       let md = node.textContent.trim()
       if (!md) return
       let doc = {_id: '', path: ''}
-      md = node.textContent.slice(0,5).trim() // todo: <<==========
+      md = md.slice(0,6) // nb: todo: <<==========
       if (/H\d/.test(node.nodeName)) {
         if (chapter.length) {
           chapter[0].size = chapter.length
@@ -96,12 +103,16 @@ async function getMDs(epub) {
         prevheader = doc
 
         // md = ['#'.repeat(doc.level), md].join(' ')
-      } else if (node.nodeName == 'P') {
+      } else if (node.nodeName === 'DIV') {
+        // log('____DIV', md)
+        return
+      } else if (node.nodeName === 'P') {
         // footnotes, endnotes:
         let pid = node.id // calibre v.2 <p id>
         if (!pid) {
           let firstel = node.firstChild // gutenberg <p><a id>
-          if (firstel && firstel.nodeName == 'A') pid = firstel.id
+          if (firstel && firstel.nodeName === 'A') pid = firstel.id
+          log('_PID', docid, 'pid:', pid, md, 'FIRST-N', firstel.nodeName)
         }
         // log('_PID', pid)
         if (fns.includes(pid)) {
@@ -128,7 +139,7 @@ async function getMDs(epub) {
       } // if nodeName
 
       doc.path = path
-      let _id = [chapterid, docid].join('-')
+      let _id = [path, docid].join('-')
       if (!doc._id) doc._id = _id
       doc.md = md
       chapter.push(doc)
@@ -137,8 +148,8 @@ async function getMDs(epub) {
     if (chapter.length) docs.push(chapter), chapter[0].size = chapter.length
     chapterid++
   }
-  log('___DOCS', docs.length)
-  // log('_FNS', fns.slice(0,5))
+  // log('___DOCS', docs.length)
+  log('_FNS', fns.slice(0,15))
   return _.flatten(docs)
 }
 
